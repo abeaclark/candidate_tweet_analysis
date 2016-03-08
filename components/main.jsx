78b9d09d14fc6952ef414a4743fa30d1bf4878dc
abe_clark <abeaclark@gmail.com>
2016-03-08 12:16:01 -0800

    chartjs rendering on page

--- components/main.jsx
+++ components/main.jsx
@@ -1,20 +1,56 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PieChart = require('./pieChart.jsx')
+var PieChart = require("react-chartjs").Pie;
+
+var socket = require('socket.io-client')();
 
 var Abe = React.createClass({
+  getInitialState: function() {
+    return {
+      value: 'Abe is hot',
+      negative: .5,
+      positive: .5
+    }
+  },
+  updateState: function(value) {
+    this.setState({value: value})
+  },
   render: function() {
     return (
       <div>
-      Just kidding
+      {this.state.value}
+      <PieChart
+        data={[
+        {
+        value: this.state.negative,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Negative"
+        },
+        {
+        value: this.state.positive,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Positive"
+        },
+        ]}
+        options={
+          {animation: false}
+        }
+        width="400"
+        height="150"/>
       </div>
     )
   }
 });
 
 
-ReactDOM.render(
+var main = ReactDOM.render(
   <Abe />,
   document.getElementById('react-mount')
 );
+
+socket.on('update', function (data) {
+  main.updateState(data);
+});
 
